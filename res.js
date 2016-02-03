34f8ebbfb5c3f0f1bef431f881783ff0a74ef87e
Tom Freestone <tomfreestone@gmail.com>
2016-02-03 18:57:36 +0000

    authorised users

--- res.js
+++ res.js
@@ -29,12 +29,21 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.resolution.helpers({
+    isOwner: function() {
+      return this.owner === Meteor.userId();
+    }
+  });
+
   Template.resolution.events({
     'click .toggle-checked': function() {
       Meteor.call("updateResolution", this._id, !this.checked);
     },
     'click .delete': function() {
       Meteor.call("deleteResolution", this._id);
+    },
+    'click .toggle-private': function() {
+      Meteor.call("setPrivate", this._id, !this.private);
     }
   });
   Accounts.ui.config({
@@ -50,21 +59,48 @@ if (Meteor.isServer) {
   });
 
   Meteor.publish("resolutions", function() {
-    return Resolutions.find();
+    return Resolutions.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
+
 }
 
 Meteor.methods({
   addResolution: function(title) {
     Resolutions.insert({
       title: title,
-      createdAt: new Date()
+      createdAt: new Date(),
+      owner: Meteor.userId()
     });
   },
   updateResolution: function(id, checked) {
+    var res = Resolutions.findOne(id);
+
+    if(res.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorised');
+    }
     Resolutions.update(id, {$set: {checked: checked}})
   },
   deleteResolution: function(id) {
+    var res = Resolutions.findOne(id);
+
+    if(res.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorised');
+    }
     Resolutions.remove(id);
+  },
+  setPrivate: function(id, private) {
+    var res = Resolutions.findOne(id);
+
+    if(res.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorised');
+    }
+    Resolutions.update(id, {$set: {private: private}})
+
+
   }
 });
